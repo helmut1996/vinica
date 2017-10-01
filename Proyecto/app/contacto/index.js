cd5bbdba2c1f7cf02e9f6e96b5a8ed02d494c503
helmut1996 <helmut-1521@hotmail.com>
2017-10-01 07:10:29 +0200

    Modificacion del proyecto

---
+++ Proyecto/app/contacto/index.js
@@ -0,0 +1,7 @@
+const contacto = `<h1>Contáctanos</h1>
+                  <p>Numero Movistar 89902369</p>
+                  <p>Numero Claro 86338407</p>
+                  
+                  `
+
+export default contacto
